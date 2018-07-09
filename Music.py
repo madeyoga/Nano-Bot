aac5a624849a9c49c94133f8b6a85cc39ce26713
MadeYoga <vngla21@gmail.com>
2018-07-09 11:45:00 +0700

    first commit

---
+++ Music.py
@@ -0,0 +1,426 @@
+import asyncio
+import discord
+from discord.ext import commands
+from discord.utils import get
+import urllib.request
+import urllib.parse
+import re
+import youtube_dl
+import simplejson
+import lxml
+from lxml import etree
+from apiclient.discovery import build
+from apiclient.errors import HttpError
+from oauth2client.tools import argparser
+import os
+if not discord.opus.is_loaded():
+    
+    discord.opus.load_opus('opus')
+
+DEVELOPER_KEY = os.environ.get('DEV_KEY')
+YOUTUBE_API_SERVICE_NAME = "youtube"
+YOUTUBE_API_VERSION = "v3"
+
+def __init__(self, bot):
+        self.bot = bot
+
+class VoiceEntry:
+    def __init__(self, message, player):
+        self.requester = message.author
+        self.channel = message.channel
+        self.player = player
+
+    def __str__(self):
+        fmt = ' {0.title}\nuploaded by {0.uploader}\nrequested by {1.display_name}'
+        duration = self.player.duration
+        if duration:
+            fmt = fmt + ' [length: {0[0]}m {0[1]}s]'.format(divmod(duration, 60))
+        return fmt.format(self.player, self.requester)
+
+class VoiceState:
+    def __init__(self, bot):
+        self.current = None
+        self.voice = None
+        self.bot = bot
+        self.play_next_song = asyncio.Event()
+        self.songs = asyncio.Queue()
+        self.skip_votes = set() # a set of user_ids that voted
+        self.audio_player = self.bot.loop.create_task(self.audio_player_task())
+        self.list_url = []
+        self.list_title = []
+        self.active_message = None
+        
+    def is_playing(self):
+        if self.voice is None or self.current is None:
+            return False
+
+        player = self.current.player
+        return not player.is_done()
+
+    @property
+    def player(self):
+        return self.current.player
+
+    def skip(self):
+        self.skip_votes.clear()
+        if self.is_playing():
+            self.player.stop()
+
+    def toggle_next(self):
+        self.bot.loop.call_soon_threadsafe(self.play_next_song.set)
+
+    async def audio_player_task(self):
+        while True:
+            self.play_next_song.clear()
+            self.current = await self.songs.get() ## get queue front 
+            await self.bot.send_message(self.current.channel, ':musical_note: Now playing' + str(self.current))
+            self.current.player.start()
+            await self.play_next_song.wait()
+
+class Music:
+    """Voice related commands.
+    Works in multiple servers at once.
+    """
+    def __init__(self, bot):
+        self.bot = bot
+        self.voice_states = {}
+
+    def get_voice_state(self, server):
+        state = self.voice_states.get(server.id)
+        if state is None:
+            state = VoiceState(self.bot)
+            self.voice_states[server.id] = state
+        return state
+
+    async def create_voice_client(self, channel):
+        voice = await self.bot.join_voice_channel(channel)
+        state = self.get_voice_state(channel.server)
+        state.voice = voice
+
+    def __unload(self):
+        for state in self.voice_states.values():
+            try:
+                state.audio_player.cancel()
+                if state.voice:
+                    self.bot.loop.create_task(state.voice.disconnect())
+            except:
+                pass
+
+    async def on_reaction_add(self, reaction, user):
+        server = reaction.message.server
+        state = self.get_voice_state(server)
+
+        if state.active_message and state.active_message.id == reaction.message.id and reaction.emoji == "➕":
+
+            if state.list_title.count == 0:
+                await self.bot.send_message(reaction.message.channel, "empty list")
+                return True
+
+            for i in range (0, 4):
+                if state.list_url.count == 0:
+                    break
+                state.list_title.pop(0)
+                state.list_url.pop(0)
+
+            embed = discord.Embed(title=server.name, description="Playlist", color=0x191970)
+            count = 0
+            for idx in range(0, 4):
+                if len(state.list_url) == count:
+                    break
+                embed.add_field(name=str(idx + 1) + ". " + state.list_title[idx], value=state.list_url[idx], inline=True)
+                count += 1
+
+            await self.bot.delete_message(reaction.message)
+
+            msg = await self.bot.send_message(reaction.message.channel, embed=embed)
+            if count == 4:
+                await self.bot.add_reaction(msg, emoji="1⃣")
+                await self.bot.add_reaction(msg, emoji="2⃣")
+                await self.bot.add_reaction(msg, emoji="3⃣")
+                await self.bot.add_reaction(msg, emoji="4⃣")
+                await self.bot.add_reaction(msg, emoji="➕")
+            else:
+                for idx in range(0, count):
+                    if idx == 0:
+                        await self.bot.add_reaction(msg, emoji="1⃣")
+                    elif idx == 1:
+                        await self.bot.add_reaction(msg, emoji="2⃣")
+                    elif idx == 2:
+                        await self.bot.add_reaction(msg, emoji="3⃣")
+                    elif idx == 3:
+                        await self.bot.add_reaction(msg, emoji="4⃣")
+
+            await self.bot.add_reaction(msg, emoji="🎵")
+            state.active_message = msg
+
+            return True
+
+        if not state.active_message is None and state.active_message.id == reaction.message.id and not reaction.emoji == "🎵":
+            emoji = reaction.emoji
+            index = 0
+            if emoji == "2⃣":
+                index = 1
+            elif emoji == "3⃣":
+                index = 2
+            elif emoji == "4⃣":
+                index = 3
+
+            await self.bot.delete_message(reaction.message)
+            state.active_message = None
+            #await self.bot.send_message(reaction.message.channel, "play #" + str(index + 1))
+
+            summoned_channel = user.voice_channel
+            if summoned_channel is None:
+                await self.bot.send_message(reaction.message.channel, 'Are you sure you are in a channel?')
+                return False
+
+            if state.voice is None:
+                state.voice = await self.bot.join_voice_channel(summoned_channel)
+            else:
+                await state.voice.move_to(summoned_channel)
+
+            try:
+                player = await state.voice.create_ytdl_player(state.list_url[index], ytdl_options=None, after=state.toggle_next)
+            except Exception as e:
+                fmt = 'An error occurred while processing this request: ```py\n{}: {}\n```'
+                await self.bot.send_message(reaction.message.channel, fmt.format(type(e).__name__, e))
+            else:
+                player.volume = 0.6
+                entry = VoiceEntry(reaction.message, player)
+                await self.bot.send_message(reaction.message.channel, ':musical_note: Enqueued ' + str(entry))
+                await state.songs.put(entry)
+            
+
+    @commands.command(pass_context=True, no_pm=True)
+    async def s(self, ctx, *args):
+        """ Search Song. """
+        key = ""
+        for word in args:
+            key += word
+            key += " "
+        server = ctx.message.server
+        state = self.get_voice_state(server)
+        if not state.active_message is None:
+            await self.bot.delete_message(state.active_message)
+        state.list_url.clear()
+        state.list_title.clear()
+
+        count = 0
+        embed = discord.Embed(title=server.name, description="Playlist", color=0x191970)
+        
+        youtube = build(YOUTUBE_API_SERVICE_NAME, YOUTUBE_API_VERSION, developerKey=DEVELOPER_KEY)
+        search_response = youtube.search().list(
+            q=key,
+            part="id,snippet",
+            maxResults=25
+        ).execute()
+
+        for search_result in search_response.get("items", []):
+            if search_result["id"]["kind"] == "youtube#video":
+                #videos.append("%s (%s)" % (search_result["snippet"]["title"], search_result["id"]["videoId"]))
+                url = "http://www.youtube.com/watch?v=" + search_result["id"]["videoId"]
+                title = search_result["snippet"]["title"]
+                
+                state.list_url.append(url)
+                state.list_title.append(title)
+
+                count += 1
+                
+        for x in range(0, 4):
+            embed.add_field(name=str(x + 1) + ". " + state.list_title[x], value=state.list_url[x], inline=True)
+
+
+        msg = await self.bot.send_message(ctx.message.channel, embed=embed)
+        await self.bot.add_reaction(msg, emoji="1⃣")
+        await self.bot.add_reaction(msg, emoji="2⃣")
+        await self.bot.add_reaction(msg, emoji="3⃣")
+        await self.bot.add_reaction(msg, emoji="4⃣")
+        await self.bot.add_reaction(msg, emoji="➕")
+        await self.bot.add_reaction(msg, emoji="🎵")
+        state.active_message = msg
+        
+    @commands.command(pass_context=True, no_pm=True)
+    async def p(self, ctx, index : int):
+        """ Picks Song from Playlist. """
+        server = ctx.message.server
+        state = self.get_voice_state(server)
+        if state.voice is None:
+            success = await ctx.invoke(self.summon)
+            #await self.bot.say(":notes: Searching :mag: {}".format(song))
+            if not success:
+                return
+        try:
+            player = await state.voice.create_ytdl_player(state.list_url[index - 1], ytdl_options=None, after=state.toggle_next)
+        except Exception as e:
+            fmt = 'An error occurred while processing this request: ```py\n{}: {}\n```'
+            await self.bot.send_message(ctx.message.channel, fmt.format(type(e).__name__, e))
+        else:
+            player.volume = 0.6
+            entry = VoiceEntry(ctx.message, player)
+            await self.bot.say(':musical_note: Enqueued ' + str(entry))
+            await state.songs.put(entry)
+            
+    @commands.command(pass_context=True, no_pm=True)
+    async def playlist(self, ctx):
+        """ Shows playlist """
+        server = ctx.message.server
+        state = self.get_voice_state(server)
+        embed = discord.Embed(title=server.name, description="Playlist", color=0x191970)
+        urls = state.list_url
+        titles = state.list_title
+        count = 0
+        for url in urls:
+            embed.add_field(name=str(count + 1) + ". " + titles[count], value=url, inline=True)
+            count += 1
+        await self.bot.say(embed=embed)
+        
+    @commands.command(pass_context=True, no_pm=True)
+    async def join(self, ctx, *, channel : discord.Channel):
+        """Joins a voice channel."""
+        try:
+            await self.create_voice_client(channel)
+        except discord.ClientException:
+            await self.bot.say('Already in a voice channel...')
+        except discord.InvalidArgument:
+            await self.bot.say('This is not a voice channel...')
+        else:
+            await self.bot.say('Ready to play audio in **' + channel.name)
+
+    @commands.command(pass_context=True, no_pm=True)
+    async def summon(self, ctx):
+        """Summons the bot to join your voice channel."""
+        summoned_channel = ctx.message.author.voice_channel
+        if summoned_channel is None:
+            await self.bot.say('Are you sure you are in a channel?')
+            return False
+
+        state = self.get_voice_state(ctx.message.server)
+        if state.voice is None:
+            state.voice = await self.bot.join_voice_channel(summoned_channel)
+        else:
+            await state.voice.move_to(summoned_channel)
+
+        return True
+
+    @commands.command(pass_context=True, no_pm=True)
+    async def play(self, ctx, *, song : str):
+        """Plays a song. Search automatically """
+        state = self.get_voice_state(ctx.message.server)
+        opts = {
+            'default_search': 'auto',
+            'quiet': True,
+        }
+
+        if state.voice is None:
+            success = await ctx.invoke(self.summon)
+            await self.bot.say(":notes: Searching :mag: {}".format(song))
+            if not success:
+                return
+
+        try:
+            player = await state.voice.create_ytdl_player(song, ytdl_options=opts, after=state.toggle_next)
+        except Exception as e:
+            fmt = 'An error occurred while processing this request: ```py\n{}: {}\n```'
+            await self.bot.send_message(ctx.message.channel, fmt.format(type(e).__name__, e))
+        else:
+            player.volume = 0.6
+            entry = VoiceEntry(ctx.message, player)
+            await self.bot.say(':musical_note: Enqueued ' + str(entry))
+            await state.songs.put(entry)
+
+    #@commands.command(pass_context=True, no_pm=True)
+    #async def queue(self, ctx):
+    #    """ Songs Queue """
+    #    state = self.get_voice_state(ctx.message.server)
+    #    embed = discord.Embed(title="Queue", description="", color=0x00ff00)
+    #    #while not songsList.empty():
+    #    songs = state.songs
+    #    entry = await state.songs.get()
+    #    #embed.add_field(name = "{}".format(state.songs.qsize()), value=entry, inline=True)
+    #    await self.bot.say("")
+        
+    @commands.command(pass_context=True, no_pm=True)
+    async def volume(self, ctx, value : int):
+        """Sets the volume of the currently playing song."""
+        state = self.get_voice_state(ctx.message.server)
+        if state.is_playing():
+            player = state.player
+            player.volume = value / 100
+            await self.bot.say('Set the volume to {:.0%}'.format(player.volume))
+            
+    @commands.command(pass_context=True, no_pm=True)
+    async def resume(self, ctx):
+        """Resumes the currently played song."""
+        state = self.get_voice_state(ctx.message.server)
+        if state.is_playing():
+            player = state.player
+            player.resume()
+            
+    @commands.command(pass_context=True, no_pm=True)
+    async def pause(self, ctx):
+        """Pause the currently played song."""
+        state = self.get_voice_state(ctx.message.server)
+        if state.is_playing():
+            player = state.player
+            player.pause()
+
+    @commands.command(pass_context=True, no_pm=True)
+    async def stop(self, ctx):
+        """Stops playing audio and leaves the voice channel.
+        This also clears the queue.
+        """
+        server = ctx.message.server
+        state = self.get_voice_state(server)
+        
+        if state.is_playing():
+            player = state.player
+            player.stop()
+
+        try:
+            state.audio_player.cancel()
+            del self.voice_states[server.id]
+            await state.voice.disconnect()
+            await self.bot.say("Cleared the queue and disconnected from voice channel ")
+        except:
+            pass
+
+    @commands.command(pass_context=True, no_pm=True)
+    async def skip(self, ctx):
+        """Vote to skip a song. The song requester can automatically skip.
+        3 skip votes are needed for the song to be skipped.
+        """
+
+        state = self.get_voice_state(ctx.message.server)
+        if not state.is_playing():
+            await self.bot.say('Not playing any music right now...')
+            return
+
+        voter = ctx.message.author
+        if voter == state.current.requester:
+            await self.bot.say('Requester requested skipping song...')
+            state.skip()
+        elif voter.id not in state.skip_votes:
+            state.skip_votes.add(voter.id)
+            total_votes = len(state.skip_votes)
+            if total_votes >= 3:
+                await self.bot.say('Skip vote passed, skipping song...')
+                state.skip()
+            else:
+                await self.bot.say('Skip vote added, currently at [{}/3]'.format(total_votes))
+        else:
+            await self.bot.say('You have already voted to skip this song.')
+
+    @commands.command(pass_context=True, no_pm=True)
+    async def playing(self, ctx):
+        """Shows info about the currently played song."""
+        state = self.get_voice_state(ctx.message.server)
+        if state.current is None:
+            await self.bot.say('Not playing anything.')
+        else:
+            skip_count = len(state.skip_votes)
+            await self.bot.say(':musical_note: Now playing {} [skips: {}/3]'.format(state.current.player.title, skip_count))
+
+
+def setup(bot):
+    bot.add_cog(Music(bot))
+    print('Music is loaded')
---
+++ Nano.py
@@ -0,0 +1,272 @@
+import discord
+from discord.ext import commands
+from discord.ext.commands import Bot
+from discord.voice_client import VoiceClient
+import youtube_dl
+from pixivpy3 import *
+import safygiphy
+import requests
+import io
+import asyncio
+import os
+import random
+import urllib.request
+import urllib.parse
+import re
+from pathlib import Path
+
+description = """ General """
+
+startup_extensions = ["Music"]
+
+# DISCORD CLIENT #
+bot = commands.Bot(command_prefix='-', description = "General")
+
+# GIPHY #
+giphy = safygiphy.Giphy()
+
+# PIXIV #
+api = AppPixivAPI()
+api.login( str(os.environ.get('PIXIV_MAIL')), str(os.environ.get('PIXIV_PASS')) )
+
+@bot.event
+async def on_ready():
+    print ('Bot online')
+    await bot.change_presence(game=discord.Game(name='-help'))
+
+@bot.event
+async def on_member_join(member):
+   role = discord.utils.get(member.server.roles, name='New Member')
+   await bot.add_roles(member, role)
+
+@bot.command()
+async def echo(*args):
+    output=""
+    for word in args:
+        output += word
+        output += " "
+    await bot.say(output)
+
+@bot.command(pass_context=True)
+async def gif(ctx, key : str):
+    """ Replies a gif from giphy, <prefix>gif <args> """
+
+    gif_tag = key
+    random_gif = giphy.random(tag=str(gif_tag))
+    response = requests.get(
+        str(random_gif.get("data", {}).get('image_original_url')), stream=True
+    )
+    if not response:
+        await bot.say("couldn't find " + gif_tag)
+        return
+    await bot.send_file(ctx.message.channel, io.BytesIO(response.raw.read()), filename='video.gif')
+    
+@bot.command(pass_context=True)
+async def pixiv(ctx, *args):
+    """ Search img, usage: <prefix>pixiv <args> """
+    key = ""
+    for word in args:
+        key += word
+        key += " "
+        
+    json_result = api.search_illust(key, search_target='partial_match_for_tags')
+    if len(json_result.illusts) == 0:
+        await bot.say("Couldn't find " + key)
+        return
+    random_number = random.randint(0, len(json_result.illusts) - 1)
+    illust = json_result.illusts[random_number]
+    await bot.say("https://www.pixiv.net/member_illust.php?mode=medium&illust_id=" + str(illust.id))
+
+@bot.command(pass_context=True)
+async def illust():
+    """ returns recommended illust from pixiv """
+    json_result = api.illust_recommended(content_type='illust')
+    random_number = random.randint(0, len(json_result.illusts) - 1)
+    illust = json_result.illusts[random_number]
+    await bot.say("https://www.pixiv.net/member_illust.php?mode=medium&illust_id=" + str(illust.id))
+
+@bot.command(pass_context=True)
+async def manga():
+    """ returns recommended manga from pixiv """ 
+    json_result = api.illust_recommended(content_type='manga')
+    random_number = random.randint(0, len(json_result.illusts) - 1)
+    illust = json_result.illusts[random_number]
+    await bot.say("https://www.pixiv.net/member_illust.php?mode=medium&illust_id=" + str(illust.id))
+
+@bot.command(pass_context=True)
+async def ranking(ctx, mode : str):
+    """ returns ranked img based on mode,'-example ranking' for details """
+    json_result = api.illust_ranking(mode='day')
+    random_number = random.randint(0, len(json_result.illusts) - 1)
+    illust = json_result.illusts[random_number]
+    await bot.say("https://www.pixiv.net/member_illust.php?mode=medium&illust_id=" + str(illust.id))
+        
+@bot.command(pass_context=True)
+async def pixivday(ctx):
+    """ (Recommended)Pixiv's image based on rank, mode: day """
+    json_result = api.illust_ranking('day')
+    random_number = random.randint(0, len(json_result.illusts) - 1)
+    illust = json_result.illusts[random_number]
+
+    await bot.say("https://www.pixiv.net/member_illust.php?mode=medium&illust_id=" + str(illust.id))
+
+@bot.command(pass_context=True)
+async def pixivweek(ctx):
+    """ (Recommended)Pixiv's image based on rank, mode: Week """
+    json_result = api.illust_ranking('week')
+    random_number = random.randint(0, len(json_result.illusts) - 1)
+    illust = json_result.illusts[random_number]
+    await bot.say("https://www.pixiv.net/member_illust.php?mode=medium&illust_id=" + str(illust.id))
+
+@bot.command(pass_context=True)
+async def pixivmonth(ctx):
+    """ returns Pixiv's image based on rank, mode: Month """
+    json_result = api.illust_ranking('month')
+    random_number = random.randint(0, len(json_result.illusts) - 1)
+    illust = json_result.illusts[random_number]
+
+    await bot.say("https://www.pixiv.net/member_illust.php?mode=medium&illust_id=" + str(illust.id))
+
+@bot.command(pass_context=True)
+async def pixivuser(ctx, user_id):
+    """ usage: <prefix>pixivuser <args>, args: user id """
+    json_result = api.user_detail(user_id)
+    user = json_result.user             #USER
+    
+    json_result = api.user_illusts(user_id)
+    if not len(json_result.illusts) == 0:
+        illust = json_result.illusts[0] #ILLUST 
+    
+    embed = discord.Embed(title="{}'s info".format(user_id), description="Pixiv", color=0x00ff00)
+    embed.add_field(name="Name", value=user.name, inline=True)
+    embed.add_field(name="User Account", value=user.account, inline=True)
+    embed.add_field(name="Request from", value=ctx.message.author, inline=True)
+    embed.add_field(name="Link", value="https://www.pixiv.net/member.php?id=" + user_id, inline=True)
+    
+    if not len(json_result.illusts) == 0:
+        await bot.say("User's link\t  : https://www.pixiv.net/member.php?id=" + user_id  + "\nLatest work\t: https://www.pixiv.net/member_illust.php?mode=medium&illust_id=" + str(illust.id))
+    else:
+        await bot.say("User's link\t  : https://www.pixiv.net/member.php?id=" + user_id)
+    
+
+@bot.command(pass_context=True)
+async def ping(ctx):
+    """Some Text Response"""
+    await bot.say(":ping_pong: PIN PON PAN PON!!")
+
+@bot.command(pass_context=True)
+async def info(ctx, user: discord.Member):
+    """Shows member's Informations"""
+    embed = discord.Embed(title="{}'s info".format(user.name), description="Here's what i could find.", color=0x00ff00)
+    embed.add_field(name="Name", value=user.name, inline=True)
+    embed.add_field(name="ID", value=user.id, inline=True)
+    embed.add_field(name="Status", value=user.status, inline=True)
+    embed.add_field(name="Highest Role", value=user.top_role, inline=True)
+    embed.add_field(name="Joined", value=user.joined_at, inline=True)
+    embed.set_thumbnail(url=user.avatar_url)
+    await bot.say(embed=embed)
+
+@bot.command(pass_context=True)
+async def fact(ctx, user: discord.Member):
+    """Some Text Response"""
+    if user.name == "SomeLikeItHot" and not user.bot:
+        await bot.say("HoT!")
+    elif user.bot:
+        await bot.say("{} is a bot".format(user.name))
+    else:
+        await bot.say("{} is gay :eggplant:".format(user.name))
+
+@bot.command(pass_context=True)
+async def kick(ctx, user: discord.Member):
+    """Kicks member"""
+    await bot.say(":boot: Cya, {}. You Loser!".format(user.name))
+    await bot.kick(user)
+
+@bot.command(pass_context=True)
+async def serverinfo(ctx):
+    embed = discord.Embed(title="{}'s info".format(ctx.message.server.name), description="Here's what i could find", color=0x00ff00)
+    embed.add_field(name="Name", value=ctx.message.server.name, inline=True)
+    embed.add_field(name="ID", value=ctx.message.server.id, inline=True)
+    embed.add_field(name="Roles", value=len(ctx.message.server.roles), inline=True)
+    embed.add_field(name="Members", value=len(ctx.message.server.members), inline=True)
+    embed.set_thumbnail(url=ctx.message.server.icon_url)
+    await bot.say(embed=embed)
+
+@bot.command(pass_context=True)
+async def clear(ctx, amount=100):
+    channel = ctx.message.channel
+    messages = []
+    async for message in bot.logs_from(channel, limit=int(amount)):
+        messages.append(message)
+    await bot.delete_messages(messages)
+    await bot.say('Messages deleted')
+    
+if __name__ == '__main__':
+    for extension in startup_extensions:
+        try:
+            bot.load_extension(extension)
+        except Exception as e:
+            exc = '{}: {}'.format(type(e).__name__, e)
+            print('Failed to load extension {}\n{}'.format(extension, exc))
+
+@bot.command(pass_context=True)
+async def example(ctx, cmd : str):
+    """ shows example usage of commands, -commands <cmd> """
+    command = ""
+    if cmd == "ping":
+        command = "-ping"
+    elif cmd == "fact":
+        command = "-fact @tag_member"
+    elif cmd == "clear":
+        command = "-clear 20\nif no args then i will delete all the messages\n"
+        command += "I Can only delete messages in the range of [2, 100] "
+        command += "and if i have the permissions"
+    elif cmd == "serverinfo":
+        command = "-serverinfo"
+    elif cmd == "example":
+        command = "-example ranking"
+    elif cmd == "info":
+        command = "-info @tag_member"
+    elif cmd == "kick":
+        command = "-kick @tag_member\n"
+        command += "I Can only kick if i have the permissions"
+    elif cmd == "gif":
+        command = "-gif fun\n"
+        command += "I only takes the first arg of the message for the tag\n"
+        command += "eg. -gif fun happy, tag: fun"
+    elif cmd == "echo":
+        command = "-echo Hello World!\nI will say what you just said"
+    elif cmd == "illust":
+        command = "-illust"
+    elif cmd == "manga":
+        command = "-manga"
+    elif cmd == "pixiv":
+        command = "-pixiv coding is fun\n"
+        command += "i will take all the args as the search key\nsearch key: 'coding is fun'"
+    elif cmd == "pixivday":
+        command = "-pixivday"
+    elif cmd == "pixivweek":
+        command = "-pixivweek"
+    elif cmd == "pixivmonth":
+        command = "-pixivmonth"
+    elif cmd == "ranking":
+        command = "-ranking week_original\n"
+        command += "mode: [day, week, month, day_male, day_female, week_original, week_rookie, day_manga]\n"
+        command += "mode r18: [day_r18, day_male_r18, day_female_r18, week_r18, week_r18g]"
+    elif cmd == "pixivuser":
+        command = "-pixivuser 12878330"
+    elif cmd == "play":
+        command = "-play asmr\n"
+        command += "usage: -play <args>, args: search key or youtube url"
+    elif cmd == "s":
+        command = "-s asmr\n"
+        command += "usage: -play <args>, args: search key\nrelated: -p, -playlist"
+    elif cmd == "playlist":
+        command = "-playlist\nplaylist will be updated everytime -s command has been called"
+    elif cmd == "p":
+        command = "-p 1\nplays song number 1 on playlist\nto check playlist, use -playlist"
+    else:
+        command = "Unknown command. Sorry, i don't know what " + cmd + " is"
+    await bot.send_message(ctx.message.channel, command)
+
+bot.run( str(os.environ.get('BOT_TOKEN')) )
