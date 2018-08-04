8efaa47a84eac5a8d8db24d04e9e7cf7fde95f46
MadeYoga <vngla21@gmail.com>
2018-08-04 22:06:19 +0700

    adding translator

--- Nano.py
+++ Nano.py
@@ -16,7 +16,7 @@ import re
 import aiohttp
 from pathlib import Path
 
-startup_extensions = ["Music", "Moderation", "Info", "gag"]
+startup_extensions = ["Music", "Moderation", "Info", "gag", "Translator"]
 
 # DISCORD CLIENT #
 bot = commands.Bot(command_prefix='.', description = "General")
