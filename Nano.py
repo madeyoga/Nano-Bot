a2ed347b4167000678b169489c947476ecd2e482
Madeoga <37206663+MadeYoga@users.noreply.github.com>
2018-08-13 23:42:13 +0700

    Update Nano.py

--- Nano.py
+++ Nano.py
@@ -16,7 +16,7 @@ import re
 import aiohttp
 from pathlib import Path
 
-startup_extensions = ["Moderation", "Info", "gag", "Translator"]
+startup_extensions = ["Moderation", "Info", "gag", "Translator", "Pixiv2"]
 
 # DISCORD CLIENT #
 bot = commands.Bot(command_prefix='n>', description = "General")
