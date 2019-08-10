8774cadb737d132af3da8b2028d615cee11b089c
MadeYoga <vngla21@gmail.com>
2019-08-10 22:56:01 +0700

    Moves DBL methods, from mainpy to NanoClient object.

--- listener/core/client.py
+++ listener/core/client.py
@@ -1,5 +1,7 @@
 import discord
 from discord.ext import commands
+import os
+import aiohttp
 
 class NanoContext(commands.Context):
     @property
@@ -14,13 +16,32 @@ class NanoClient(commands.Bot):
         self.id = id
         self.owner_id = owner_id
         self.command_prefix = command_prefix
+        
+        # Discord Bot List updates.
+        self.dbl_token = os.environ['DBL_TOKEN']
+        self.BASE_URL = "https://discordbots.org/api/bots/458298539517411328/stats"
+        self.headers = {"Authorization": self.dbl_token}
+
+    async def update_status_on_dbl(self):
+        payload = {"server_count" : len(super(NanoClient, self).guilds)}
+        async with aiohttp.ClientSession() as session:
+            await session.post(self.BASE_URL, data=payload, headers=self.headers)
 
     async def on_ready(self):
-        print('Logged on as {0}!'.format(self.user))
+        status_message = discord.Game("n>help")
+        await super(NanoClient, self).change_presence(status=discord.Status.online, activity=status_message)
+        await self.update_status_on_dbl()
+        print("Logged in as {}".format(super(NanoClient, self).user))
+    
+    async def on_guild_join(self):
+        """Updates DBL when client joins a guild"""
+        await self.update_status_on_dbl()
 
-#     async def on_message(self, message):
-#         if message.author.bot:
-#             return
-#         # print('Message from {0.author}: {0.content}'.format(message))
-#         ctx = await self.get_context(message, cls=NanoContext)
-#         await self.invoke(ctx)
+    async def on_guild_leave(self):
+        """Updates DBL when client leaves a guild"""
+        await self.update_status_on_dbl()
+
+    async def on_message(self, message):
+        if message.author.bot:
+            return
+        await self.invoke(await self.get_context(message, cls=NanoContext))
