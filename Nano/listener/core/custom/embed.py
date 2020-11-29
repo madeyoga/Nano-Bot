e2fd3262e027396d073af7badacd7fc1a7aa309a
MadeYoga <vngla21@gmail.com>
2020-11-29 17:47:51 +0700

    Change custom embed color

--- Nano/listener/core/custom/embed.py
+++ Nano/listener/core/custom/embed.py
@@ -5,4 +5,4 @@ from discord import Embed
 class CustomEmbed(Embed):
 
     def __init__(self):
-        super().__init__(color=discord.Color(value=11735575).orange())
+        super().__init__(color=discord.Color(value=1).lighter_grey())
