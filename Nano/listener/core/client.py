c9176a75d8ad59c1ce448e274146f0c0247b7674
MadeYoga <vngla21@gmail.com>
2020-12-12 11:48:45 +0700

    Add sharded client

--- Nano/listener/core/client.py
+++ Nano/listener/core/client.py
@@ -10,7 +10,7 @@ class NanoContext(commands.Context):
         return 'this is my secret'
 
 
-class NanoClient(commands.Bot):
+class NanoClient(commands.AutoShardedBot):
     def __init__(self, name='Nano', id=536892183404478483,
                  owner_id=213866895806300161, command_prefix='do.', intents=None):
         super(NanoClient, self).__init__(command_prefix=command_prefix,
