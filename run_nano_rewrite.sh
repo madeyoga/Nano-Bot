eba3a92076b9ac92af9dc31adb07b20b4392a8ee
root <root@localhost.localdomain>
2020-12-17 04:37:38 +0000

    Update gitignore & requirements & bash command

--- run_nano_rewrite.sh
+++ run_nano_rewrite.sh
@@ -1,3 +1,5 @@
+cp nohup.out logs/nohup-"`date +"%Y-%m-%d"`".out
+rm nohup.out
 git pull origin master
 pip3 install -r requirements.txt
 env BOT_TOKEN=$BOT_TOKEN DEVELOPER_KEY=$DEVELOPER_KEY  REDDIT_CLIENT_ID=$REDDIT_CLIENT_ID REDDIT_CLIENT_SECRET=$REDDIT_CLIENT_SECRET REDDIT_USER_AGENT='$REDDIT_USER_AGENT' nohup python3.6 -u Nano/main.py &
