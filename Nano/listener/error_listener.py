7f2f6fec56ce24d922eafe6b410aa27b9beccffb
MadeYoga <vngla21@gmail.com>
2020-12-18 19:56:18 +0700

    Update error listener

--- Nano/listener/error_listener.py
+++ Nano/listener/error_listener.py
@@ -13,5 +13,8 @@ class ErrorListener(commands.Cog):
             return await ctx.send(":x: | This command is potentially nsfw and can only be used in nsfw channel.")
         if isinstance(error, commands.errors.CommandOnCooldown):
             return await ctx.send(":x: | You are on cooldown. Try again in 1 second")
-        else:
-            print(error)
+        if isinstance(error, commands.errors.NotOwner):
+            return await ctx.send(":x: | You do not own this bot, ok?")
+
+        traceback.print_stack()
+        traceback.print_exc()
