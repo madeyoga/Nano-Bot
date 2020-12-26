1c24178e731c945cc68976aacfa6a36692146b53
MadeYoga <vngla21@gmail.com>
2020-12-26 14:32:23 +0700

    Revert solution for audio speed issue

--- Nano/listener/core/music/audio_source.py
+++ Nano/listener/core/music/audio_source.py
@@ -24,7 +24,7 @@ ytdl_format_options = {
 }
 
 ffmpeg_options = {
-    'before_options': '-use_wallclock_as_timestamps 1 -reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5',
+    'before_options': '-re -reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5',
     'options': '-vn -nostats -loglevel 0'
 }
 
