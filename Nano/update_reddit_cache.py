1e4d4c7c08d2162f9ccdbf85050138594a4114a6
MadeYoga <vngla21@gmail.com>
2021-08-17 18:22:31 +0700

    Fix nsfw issues, filter all nsfw contents

--- Nano/update_reddit_cache.py
+++ Nano/update_reddit_cache.py
@@ -17,38 +17,40 @@ reddit = praw.Reddit(
 
 for key in subreddit_dictionary:
     subreddit_name = subreddit_dictionary.get(key)
-
-    submissions = list(reddit.subreddit(subreddit_name).hot())
+    try:
+        submissions = list(reddit.subreddit(subreddit_name).hot())
+    except:
+        continue
     shuffle(submissions)
 
     dump_list = []
     for submission in submissions:
-        temp = submission.__dict__
+        if not submission.over_18:
+            temp = submission.__dict__
 
-        post_hint = temp.get('post_hint')
-        if post_hint != 'image':
-            continue
+            post_hint = temp.get('post_hint')
+            if post_hint != 'image':
+                continue
 
-        if submission.author is not None:
-            author = submission.author.name
-        else:
-            author = None
+            if submission.author is not None:
+                author = submission.author.name
+            else:
+                author = None
 
-        permalink = temp.get('permalink')
-
-        submission_dict = {
-            'title': submission.title,
-            'author': author,
-            'subreddit_name_prefixed': temp.get('subreddit_name_prefixed'),
-            'post_hint': post_hint,
-            'url': temp.get('url'),
-            'url_overridden_by_dest': temp.get('url_overridden_by_dest'),
-            'over_18': submission.over_18,
-            'permalink': f"https://www.reddit.com{permalink}",
-            'score': submission.score,
-            'original_content': submission.is_original_content
-        }
-        dump_list.append(submission_dict)
+            permalink = temp.get('permalink')
+            submission_dict = {
+                'title': submission.title,
+                'author': author,
+                'subreddit_name_prefixed': temp.get('subreddit_name_prefixed'),
+                'post_hint': post_hint,
+                'url': temp.get('url'),
+                'url_overridden_by_dest': temp.get('url_overridden_by_dest'),
+                'over_18': submission.over_18,
+                'permalink': f"https://www.reddit.com{permalink}",
+                'score': submission.score,
+                'original_content': submission.is_original_content
+            }
+            dump_list.append(submission_dict)
 
     filepath = f'listener/cache/{subreddit_name}.json'
     with open(filepath, 'w+') as file:
